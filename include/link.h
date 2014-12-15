57c5a3269f3b0b02e2f1b7d72891298008d27602
Wisam Reid <wisamreid@gmail.com>
2014-12-14 19:13:11 -0800

    compiles all white output
    Signed-off-by: Wisam Reid <wisamreid@gmail.com>

--- include/link.h
+++ include/link.h
@@ -4,6 +4,20 @@
 #include <Eigen/Core>
 #include "joint.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <sys/time.h>
+#endif
+
+#ifdef OSX
+#include <GLUT/glut.h>
+#include <OpenGL/glu.h>
+#else
+#include <GL/glut.h>
+#include <GL/glu.h>
+#endif
+
 // stl :: string theory labs (taking over a universe near you)
 namespace stl
 {
--- src/joint.cpp
+++ src/joint.cpp
@@ -1,5 +1,19 @@
 #include "joint.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <sys/time.h>
+#endif
+
+#ifdef OSX
+#include <GLUT/glut.h>
+#include <OpenGL/glu.h>
+#else
+#include <GL/glut.h>
+#include <GL/glu.h>
+#endif
+
 using namespace stl;
 
 Joint::Joint() : childLink(NULL), type(UNIVERSAL) {}
@@ -11,4 +25,29 @@ void Joint::draw() {
   //Assuming you have the Thetas, (I will add them in late, but for now feel free to hard code thetas in this function), use those thetas to rotate around the axes of rotions and update hte OpenGL transform stack based on the new rotion.
 
   if (childLink) childLink->draw();
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glRotatef(0.0f, 0.0f, 1.0f, 0.0f);
+
+    glBegin(GL_TRIANGLES);
+
+    glColor3f(1.0f, 0.0f, 0.0f); //FRONT
+    glVertex3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+
+    glColor3f(0.0f, 1.0f, 0.0f); //RIGHT
+    glVertex3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(0.0f, -1.0f, -1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+
+    glColor3f(0.0f, 0.0f, 1.0f); //LEFT
+    glVertex3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    glVertex3f(0.0f, -1.0f, -1.0f);
+
+    glEnd();
 }
