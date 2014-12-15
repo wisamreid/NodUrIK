169feee4c8bc2aef47acdaf01f36bdd0f2d8cb7c
EdsterG <eddie_g_89@hotmail.com>
2014-12-14 18:52:53 -0800

    Added DOFS to joints

--- include/joint.h
+++ include/joint.h
@@ -10,10 +10,14 @@
 namespace stl
 {
 
+  typedef Eigen::Matrix<double,1,Dynamic> Dofs;
+  typedef Eigen::Matrix<double,3,Dynamic> Jacobian;
+
   enum JointType {
-    UNIVERSAL,
-    SOMETHING,
-    PISTON
+    BALL,         // 3 axes of rotation
+    UNIVERSAL,    // 2 axes of rotation
+    HINGE,        // 1 axis of rotation
+    SLIDER        // 0 axis of rotation, 1 axis of translation
   };
 
   class Link;
@@ -23,7 +27,7 @@ namespace stl
     Link* childLink;
     Eigen::Vector3d anchor; // Relative to the tip of the parent link
     std::vector<Eigen::Vector3d> axes; // Relative to the tip of the parent link
-    //double* thetas;
+    std::vector<double> thetas;
     JointType type;
 
   public:
@@ -31,6 +35,8 @@ namespace stl
     Joint(Link* child_, JointType type_);
 
     void draw();
+    int GetNumDOFS();
+    void SetDOFS(Dofs& dofs, int& startIndex);
   };
 
 }
