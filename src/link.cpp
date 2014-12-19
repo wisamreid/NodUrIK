24d4b36b0652c71b6a24ba9181dbf356fe9d87cb
EdsterG <eddie_g_89@hotmail.com>
2014-12-18 21:02:15 -0800

    Fixed cone to be fully solid

--- src/link.cpp
+++ src/link.cpp
@@ -2,10 +2,34 @@
 
 using namespace stl;
 
+#define NUM_SLICES 30
+#define NUM_STACKS 10
+
+float link_amb[3] = {0.7f, 0.7f, 0.7f};
+float link_diff[3] = {0.7f, 0.7f, 0.7f};
+float link_spec[3] = {0.5f, 0.5f, 0.5f};
+
 Link::Link(const double length_, const double width_, Joint* child_) : length(length_), width(width_), childJoint(child_) {}
 
 void Link::draw() {
-  glutSolidCone(width, length, 30, 30);
+  glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, link_amb);
+  glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, link_diff);
+  glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, link_spec);
+
+  // Draw cone
+  glutSolidCone(width, length, NUM_SLICES, NUM_STACKS);
+
+  GLfloat x,y;
+  // Draw bottom
+  glBegin(GL_TRIANGLE_FAN);
+  glVertex3f(0.0f, 0.0f, 0.0f);
+  for (GLfloat angle = 0.0f; angle<(2.0f*M_PI)+(M_PI/NUM_SLICES*2); angle+=(M_PI/NUM_SLICES*2)) {
+    x = width*sin(angle);
+    y = width*cos(angle);
+    glVertex3f(x, y, 0.0f);
+  }
+  glEnd();
+
   if (childJoint) {
     glTranslatef(0,0,length);
     childJoint->draw();
